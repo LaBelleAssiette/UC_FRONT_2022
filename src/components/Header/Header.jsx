9b960b70f9021b989dc84f3c9ab2a441da0e5ac1
Thibault Stocker <thibault.stocker@gmail.com>
2022-07-31 22:18:16 +0200

    clean react trace

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, {} from 'react';
 import { Link } from "react-router-dom";
 
-function Header(props) {
-    // const [isOnline, setIsOnline] = useState(null);
-
-    // useEffect(() => {    function handleStatusChange(status) {      setIsOnline(status.isOnline);    }    ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange);    return () => {      ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange);    };  });
+function Header() {
     return (
         <header>
             <ul>
-                <li><Link to="/menus">Menus</Link></li>
                 <li><Link to="/chefs">Chefs</Link></li>
             </ul>
         </header>
--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import {
 } from "react-router-dom";
 import './index.css';
 
-import reportWebVitals from './reportWebVitals';
 import Header from "./components/Header/Header";
-import Menus from "./routes/menus";
 import Chefs from "./routes/chefs";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,15 +16,9 @@ root.render(
       <BrowserRouter>
           <Header />
           <Routes>
-              <Route path="/" element={<Menus />} />
-              <Route path="menus" element={<Menus />} />
+              <Route path="/" element={<Chefs />} />
               <Route path="chefs" element={<Chefs />} />
           </Routes>
       </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
